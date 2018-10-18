d8ac1d3ac9d57fdb2c703b888bf82f093a1423f1
Nicolas Ming <ncming@ucsc.edu>
2018-10-17 20:13:47 -0700

    commit to pull

--- mcts_vanilla.py
+++ mcts_vanilla.py
@@ -8,7 +8,6 @@ explore_faction = 2.
 
 def traverse_nodes(node, board, state, identity):
     """ Traverses the tree until the end criterion are met.
-
     Args:
         node:       A tree node from which the search is traversing.
         board:      The game setup.
@@ -16,59 +15,62 @@ def traverse_nodes(node, board, state, identity):
         identity:   The bot's identity, either 'red' or 'blue'.
 
     Returns:        A node from which the next stage of the search can proceed.
-
     """
+
+    print(node)
+    print(board)
+    print(state)
+    print(identity)
+
     pass
     # Hint: return leaf_node
 
 
 def expand_leaf(node, board, state):
     """ Adds a new leaf to the tree by creating a new child node for the given node.
-
     Args:
         node:   The node for which a child will be added.
         board:  The game setup.
         state:  The state of the game.
 
     Returns:    The added child node.
-
     """
+
+
+    return new_node
     pass
     # Hint: return new_node
 
 
 def rollout(board, state):
     """ Given the state of the game, the rollout plays out the remainder randomly.
-
     Args:
         board:  The game setup.
         state:  The state of the game.
-
     """
+    return choice(board.legal_actions(state))
     pass
 
 
 def backpropagate(node, won):
     """ Navigates the tree from a leaf node to the root, updating the win and visit count of each node along the path.
-
     Args:
         node:   A leaf node.
         won:    An indicator of whether the bot won or lost the game.
-
     """
+
     pass
 
 
 def think(board, state):
     """ Performs MCTS by sampling games and calling the appropriate functions to construct the game tree.
-
     Args:
         board:  The game setup.
         state:  The state of the game.
 
     Returns:    The action to be taken.
-
     """
+
     identity_of_bot = board.current_player(state)
     root_node = MCTSNode(parent=None, parent_action=None, action_list=board.legal_actions(state))
 
